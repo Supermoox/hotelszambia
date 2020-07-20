3de60e713c0d88dc5c2c7e19300a2c9362c4faff
Supermoox <mooxmk@yahoo.co.uk>
2020-07-21 00:07:59 +0200

    configured s3

--- app/models/image_room.rb
+++ app/models/image_room.rb
@@ -1,6 +1,11 @@
 class ImageRoom < ApplicationRecord
   belongs_to :room
 
-  has_attached_file :image, styles: { medium: "300x300>"}
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
