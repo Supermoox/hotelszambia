dab8f515b9274f3418fcfe8ed44f973a1d2cec9d
Supermoox <mooxmk@yahoo.co.uk>
2020-07-20 23:33:26 +0200

    setting up s3

--- app/models/image_accommodation.rb
+++ app/models/image_accommodation.rb
@@ -1,6 +1,11 @@
 class ImageAccommodation < ApplicationRecord
   belongs_to :accommodation
 
-  has_attached_file :image, styles: { medium: "300x300>"}
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- app/models/wallpaper.rb
+++ app/models/wallpaper.rb
@@ -1,7 +1,12 @@
 class Wallpaper < ApplicationRecord
   belongs_to :accommodation
 
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
 
-  has_attached_file :image, styles: { large: "760x300>"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
